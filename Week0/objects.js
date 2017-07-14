d4bc48ee9e74b38017a8a7e381fd120fe7abfc0c
AdeolaASC4 <adeolaebunlomo@gmail.com>
2017-07-14 12:15:10 -0400

    learning objects

---
+++ Week0/objects.js
@@ -0,0 +1,44 @@
+var charizard = {
+    "attack" : "Blaze",
+    "HP" : 266,
+    "legendary" : false,
+    "types" : ["Fire", "Flying"]
+    }
+
+    /*
+    -we want to add:
+    -power
+    -sidekick
+    -race
+    -gender
+    -hideout
+    -arch nemisis
+    -what universe
+    */
+    function Superhero(realName, power, sidekick, race, gender, hideout, nemisis, universe){
+        this.realName = realName;
+        this.power = power;
+        this.sidekick = sidekick;
+        this.race = race;
+        this.gender = gender;
+        this.hideout = hideout;
+        this.nemisis = nemisis;
+        this.universe = universe;
+
+        this.talk = function(){
+            console.log( "Hello I am " + this.realName)
+        }
+    }
+
+    var superman = new Superhero('Clark Kent', 'Heat vision', false, 
+                                'Kryptonian', 'Male', 'Metroplis',  
+                                'Lex Luthor', 'D.C');
+    
+
+    function Pizza(sauce, cheese, toppings, size){
+        this.sauce = sauce;
+        this.cheese = cheese;
+        this.toppings = 
+        this.size = size;
+
+    }
